0f3c2827a7e3b2fc5f193817de65423d51fe36f7
Remington Thurber <remingtonthurber@protonmail.com>
2021-12-13 21:49:54 -0700

    Adds a Drawer and makes the AppBar responsive.

--- src/Root.jsx
+++ src/Root.jsx
@@ -1,32 +1,104 @@
 import {useState} from 'react';
 import {Outlet, useLocation, useNavigate} from 'react-router-dom';
-import {AppBar, Box, Button, Tabs, Tab, Toolbar, Typography} from '@mui/material';
-import {Phone as PhoneIcon} from '@mui/icons-material';
+import {
+  AppBar,
+  Box,
+  Button,
+  IconButton,
+  List,
+  ListItem,
+  ListItemIcon,
+  ListItemText,
+  SwipeableDrawer,
+  Tabs,
+  Tab,
+  Toolbar,
+  Typography,
+  useMediaQuery
+} from '@mui/material';
+import {Home as HomeIcon, Menu as MenuIcon, AddIcCall as AddIcCallIcon, Redeem as RedeemIcon} from '@mui/icons-material';
 import Subscribe from './Subscribe';
 
 const Root = () => {
+  const lt650 = useMediaQuery('(max-width: 650px)');
+  const lt400 = useMediaQuery('(max-width: 400px)');
+
   const location = useLocation();
   const navigate = useNavigate();
 
+  const [openDrawer, setOpenDrawer] = useState(false);
   const [openSubscribe, setOpenSubscribe] = useState(false);
 
   return (
     <>
-      <AppBar>
+      <AppBar position="sticky">
         <Toolbar>
+          {lt650 &&
+          <>
+            <IconButton
+              sx={{mr: 2}}
+              edge="start"
+              color="inherit"
+              aria-label="Menu of pages"
+              onClick={() => {
+                setOpenDrawer(true);
+              }}
+            >
+              <MenuIcon/>
+            </IconButton>
+            <SwipeableDrawer
+              anchor="left"
+              open={openDrawer}
+              onOpen={() => {
+                setOpenDrawer(true);
+              }}
+              onClose={() => {
+                setOpenDrawer(
+                  false);
+              }}
+            >
+              <List sx={{minWidth: '250px'}}>
+                <ListItem
+                  button
+                  onClick={() => {
+                    navigate('/');
+                    setOpenDrawer(false);
+                  }}
+                >
+                  <ListItemIcon><HomeIcon/></ListItemIcon>
+                  <ListItemText primary="Home"/>
+                </ListItem>
+                <ListItem
+                  button
+                  onClick={() => {
+                    navigate('/support');
+                    setOpenDrawer(false);
+                  }}
+                >
+                  <ListItemIcon><RedeemIcon/></ListItemIcon>
+                  <ListItemText primary="Donate"/>
+                </ListItem>
+              </List>
+            </SwipeableDrawer>
+          </>
+          }
           <Typography sx={{mr: 1}} variant="h6">Friends of Sophia Women's Center</Typography>
-          <Tabs
-            textColor="secondary"
-            indicatorColor="secondary"
-            value={location.pathname.includes('support') ? '/support' : '/'}
-            onChange={(e, newValue) => {
-              navigate(newValue);
-            }}
-          >
-            <Tab value="/" label="Home"/>
-            <Tab value="/support" label="Donate"/>
-          </Tabs>
-          <Box sx={{flex: 1}}/>
+          {!lt650 &&
+          <>
+            <Tabs
+              textColor="secondary"
+              indicatorColor="secondary"
+              value={location.pathname.includes('support') ? '/support' : '/'}
+              onChange={(e, newValue) => {
+                navigate(newValue);
+              }}
+            >
+              <Tab value="/" label="Home"/>
+              <Tab value="/support" label="Donate"/>
+            </Tabs>
+            <Box sx={{flex: 1}}/>
+          </>
+          }
           <Button
             color="secondary"
             variant="outlined"
@@ -34,7 +106,7 @@ const Root = () => {
               setOpenSubscribe(true);
             }}
           >
-            <PhoneIcon/>&nbsp;Subscribe
+            <AddIcCallIcon/>&nbsp;{!lt400 && 'Subscribe'}
           </Button>
           <Subscribe
             open={openSubscribe}
@@ -44,7 +116,6 @@ const Root = () => {
           />
         </Toolbar>
       </AppBar>
-      <Toolbar/>
       <Box sx={{width: '100%', height: '100%'}}>
         <Outlet/>
       </Box>
